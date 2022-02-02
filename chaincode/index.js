9868921acd5c91116a791fa6367f81ad023b6069
Debraj Paul <pauldebraj7@gmail.com>
2022-02-02 18:03:51 +0530

    bugfix

--- chaincode/index.js
+++ chaincode/index.js
@@ -1,9 +1,9 @@
 'use strict';
 //Users Organization
-const userContract = require('./user-contract.js');
+const userContract = require('./user-contract');
 
 //Registrar Organization
-const registrarContract = require('./registrar-contract.js');
+const registrarContract = require('./register-contract');
 
 //Export the contracts
 module.exports.contracts = [userContract, registrarContract];
